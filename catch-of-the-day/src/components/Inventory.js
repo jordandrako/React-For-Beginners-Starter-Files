d7c1540a5220bebff7d1cea4d1edbc865f112df1
Jordan Janzen <jordancjanzen@gmail.com>
2017-08-27 21:30:51 -0600

    Day 2

--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
+import AddFishForm from "./AddFishForm";
 
 class Inventory extends Component {
   render() {
-    return <div />;
+    return (
+      <div>
+        <h2>Inventory</h2>
+        <AddFishForm addFish={this.props.addFish} />
+        <button onClick={this.props.loadSamples}>Load Example Fishes</button>
+      </div>
+    );
   }
 }
 
--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -1,8 +1,32 @@
 import React, { Component } from "react";
+import { formatPrice } from "../helpers";
 
 class Order extends Component {
   render() {
-    return <div />;
+    const orderIds = Object.keys(this.props.order);
+
+    // /// INVESTIGATE /// //
+    const total = orderIds.reduce((prevTotal, key) => {
+      const fish = this.props.fishes[key];
+      const count = this.props.order[key];
+      const isAvailable = fish && fish.status === "available";
+      if (isAvailable) {
+        return prevTotal + (count * fish.price || 0);
+      }
+      return prevTotal;
+    }, 0);
+
+    return (
+      <div className="order-wrap">
+        <h2>Your Order</h2>
+        <ul className="order">
+          <li className="total">
+            <strong>Total:</strong>
+            {formatPrice(total)}
+          </li>
+        </ul>
+      </div>
+    );
   }
 }
 
